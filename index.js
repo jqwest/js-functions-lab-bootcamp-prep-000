82202d0daaadda70d6cef616fcb536ccbf8bb794
john L <johnleavell@gmail.com>
2019-09-03 02:30:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 
 
 function happyHolidays() {
-  return ("Happy holidays!")
+  
 }
 
 function happyHolidaysTo(John) {
-  return `Happy holidays, ${John}!`;}
+  }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+  
 
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`;
+  
 }
